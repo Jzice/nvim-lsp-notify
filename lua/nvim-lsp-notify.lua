5cebd5c10d393ff77b25353476391f833212eac1
Justice <zhengyi.zhu.hust@gmail.com>
2022-05-21 10:01:53 +0800

    update

--- lua/nvim-lsp-notify.lua
+++ lua/nvim-lsp-notify.lua
@@ -37,7 +37,7 @@ local function show_notification(key, title, message, level)
 	end
 end
 
-local function on_progress(err, msg, info)
+local function on_progress2(err, msg, info)
 
 	local key = tostring(info.client_id)
 	local lsp_name = vim.lsp.get_client_by_id(info.client_id).name
@@ -134,51 +134,51 @@ end
 
 --local function setup_lsp_notify_status()
     -- vim.lsp.handlers["$/progress"] = function(_, result, ctx)
-     local function on_progress2(_, result, ctx)
-        local client_id = ctx.client_id
+local function on_progress(_, result, ctx)
+    local client_id = ctx.client_id
 
-        local val = result.value
+    local val = result.value
 
-        if not val.kind then
-            return
-        end
-
-        local notif_data = get_notif_data(client_id, result.token)
-
-        if val.kind == "begin" then
-            local message = format_message(val.message, val.percentage)
-
-            notif_data.notification = vim.notify(message, "info", {
-                title = format_title(val.title, vim.lsp.get_client_by_id(client_id).name),
-                icon = spinner_frames[1],
-                timeout = false,
-                hide_from_history = false,
-            })
-
-            notif_data.spinner = 1
-            update_spinner(client_id, result.token)
-        elseif val.kind == "report" and notif_data then
-            notif_data.notification = vim.notify(format_message(val.message, val.percentage), "info", {
-                replace = notif_data.notification,
-                hide_from_history = false,
-            })
-        elseif val.kind == "end" and notif_data then
-            notif_data.notification =
-            vim.notify(val.message and format_message(val.message) or "Complete", "info", {
-                icon = "",
-                replace = notif_data.notification,
-                timeout = 3000,
-            })
-
-            notif_data.spinner = nil
-        end
+    if not val.kind then
+        return
     end
+
+    local notif_data = get_notif_data(client_id, result.token)
+
+    if val.kind == "begin" then
+        local message = format_message(val.message, val.percentage)
+
+        notif_data.notification = vim.notify(message, "info", {
+            title = format_title(val.title, vim.lsp.get_client_by_id(client_id).name),
+            icon = spinner_frames[1],
+            timeout = false,
+            hide_from_history = false,
+        })
+
+        notif_data.spinner = 1
+        update_spinner(client_id, result.token)
+    elseif val.kind == "report" and notif_data then
+        notif_data.notification = vim.notify(format_message(val.message, val.percentage), "info", {
+            replace = notif_data.notification,
+            hide_from_history = false,
+        })
+    elseif val.kind == "end" and notif_data then
+        notif_data.notification =
+        vim.notify(val.message and format_message(val.message) or "Complete", "info", {
+            icon = "",
+            replace = notif_data.notification,
+            timeout = 3000,
+        })
+
+        notif_data.spinner = nil
+    end
+end
 
 function M.setup(opts)
 	options = vim.tbl_deep_extend('force', options, opts or {})
 
 	if not is_installed() then
-		vim.lsp.handlers['$/progress'] = on_progress2
+		vim.lsp.handlers['$/progress'] = on_progress
 	end
 
 	--setup_lsp_notify_status()
